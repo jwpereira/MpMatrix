624e65f893e281b544d09ae48d5c66722d139984
Johan Pereira <jwpereira@live.com>
2018-03-07 21:11:21 -0500

    updated main demo

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ int main(int argc, char *argv[]) {
     std::cout << "Before:\n";
     std::cout << m << std::endl;
 
+<<<<<<< HEAD
     mp_bitcnt_t m_cholesky_prec = 256;
     MpMatrix m_cholesky(m_dim, m_cholesky_prec);
 
@@ -32,6 +33,14 @@ int main(int argc, char *argv[]) {
     } else {
         std::cout << "An error occured:\n";
     }    
+=======
+    mp_bitcnt_t m_cholesky_prec = mpmatrix::bitsFromDigits(1000);
+    MpMatrix m_cholesky(m_dim, m_cholesky_prec);
+    mpmatrix::cholesky(m, m_cholesky);
+
+    std::cout << "After:\n";
+    std::cout << m_cholesky << std::endl;
+>>>>>>> 601bc4f... updated main demo
 
     return 0;
 }
