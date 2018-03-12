48c32797a19a8751647606956c3ca9c89bc9e7a7
Johan Pereira <jwpereira@live.com>
2018-03-11 23:52:30 -0400

    added ostream friendliness and casting to fixedmpz

--- src/fixedmpz.hpp
+++ src/fixedmpz.hpp
@@ -1,17 +1,25 @@
 #pragma once
 
 #include <gmpxx.h>
+#include <iostream>
 
 namespace mpmatrix {
     class fixedmpz {
       private:
         mpz_class number;
         mp_bitcnt_t dot;
+        
       public:
         fixedmpz(mpz_class number, mp_bitcnt_t dot) : number(number), dot(dot) {}
 
-        operator mpz_class*() {
-          return &(this->number);
+        operator mpz_class() {
+            return this->number;
         }
+
+        friend std::ostream &operator<<(std::ostream &os, const fixedmpz fmp);
     };
+
+    std::ostream &operator<<(std::ostream &os, const fixedmpz fmp) {
+        return os << fmp.number;
+    }
 }
