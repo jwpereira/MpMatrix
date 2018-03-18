cc9c44ed709982e0af33037afdc2b0a4b4c7aa91
Johan Pereira <jwpereira@live.com>
2018-03-18 18:17:47 -0400

    overloaded basic operators for fixedmpz

--- src/fixedmpz.hpp
+++ src/fixedmpz.hpp
@@ -10,7 +10,8 @@ namespace mpmatrix {
         mp_bitcnt_t scale;
 
       public:
-        fixedmpz(mpz_class number, mp_bitcnt_t scale) : number(number << scale), scale(scale) {}
+        fixedmpz(mpz_class number, mp_bitcnt_t scale) : number(number), scale(scale) {}
+        fixedmpz(mpz_class number) : fixedmpz(number, 0) {}
 
         mp_bitcnt_t getScale() const {
             return this->scale;
@@ -28,10 +29,52 @@ namespace mpmatrix {
             return this->number;
         }
 
+        fixedmpz operator+=(const fixedmpz &addend) {
+            this->number += addend.number;
+            return *this;
+        }
+
+        fixedmpz operator-=(const fixedmpz &subtrahend) {
+            this->number -= subtrahend.number;
+            return *this;
+        }
+
+        fixedmpz operator*=(const fixedmpz &multiplicand) {
+            this->number >>= this->scale;
+            this->number *= (multiplicand.number >> multiplicand.scale);
+
+            return *this;
+        }
+
+        fixedmpz operator/=(const fixedmpz &divisor) {
+            this->number /= divisor.number;
+            return *this;
+        }
+
         friend std::ostream &operator<<(std::ostream &os, const fixedmpz fmp);
     };
 
+    inline fixedmpz operator+(fixedmpz lhs, const fixedmpz &rhs) {
+        lhs += rhs;
+        return lhs;
+    }
+
+    inline fixedmpz operator-(fixedmpz lhs, const fixedmpz &rhs) {
+        lhs -= rhs;
+        return lhs;
+    }
+
+    inline fixedmpz operator*(fixedmpz lhs, const fixedmpz &rhs) {
+        lhs *= rhs;
+        return lhs;
+    }
+
+    inline fixedmpz operator/(fixedmpz lhs, const fixedmpz &rhs) {
+        lhs /= rhs;
+        return lhs;
+    }
+
     inline std::ostream &operator<<(std::ostream &os, const fixedmpz fmp) {
-        return os << fmp.number;
+        return os << fmp.number << ">>" << fmp.scale;
     }
 }
