5c3bef1a6e5fc8cfa7f37e5173ac41fd726c18ec
Johan Pereira <jwpereira@live.com>
2018-05-07 00:26:41 -0400

    moved demos to a new file to clear up main.cpp

---
+++ src/demo.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <iostream>
+#include <gmpxx.h>
+#include "fixedmpz.hpp"
+#include "mpmatrix.hpp"
+#include "moment_algorithm.hpp"
+
+using namespace momentmp;
+
+void demo(MpMatrix &m, size_t dim, fmp_shift_t m_shift) {
+    std::cout << "original:\n";
+    std::cout << m;
+
+    cholesky_decompose(m);
+    std::cout << "cholesky:\n";
+    std::cout << m;
+
+    MpMatrix lt(m);
+    transpose(lt);
+    std::cout << "after transpose:\n";
+    std::cout << lt;
+
+    reorient(m);
+    std::cout << "reoriented:\n";
+    std::cout << m;
+
+    MpArray diagonals(dim, m_shift);
+    extract_diagonal(m, diagonals);
+    std::cout << "diagonals:\n";
+    std::cout << diagonals;
+    std::cout << "lower:\n";
+    std::cout << m;
+
+    // reorient(m);
+    invert(m);
+    std::cout << "inverted:\n";
+    std::cout << m;
+
+    invert_diagonal(diagonals);
+    std::cout << "diagonals inverted:\n";
+    std::cout << diagonals;
+
+    invert(m);
+    std::cout << "inverted inverted:\n";
+    std::cout << m;
+
+    invert_diagonal(diagonals);
+    std::cout << "diagonals inverted inverted:\n";
+    std::cout << diagonals;
+}
+
+void demo_multiply(MpMatrix &m, size_t dim, fmp_shift_t m_shift) {
+    MpMatrix l(m);
+    cholesky_decompose(l);
+    std::cout << "cholesky ld:\n" << l << std::endl;
+
+    MpArray diagonal(dim, m_shift);
+    extract_diagonal(l, diagonal);
+
+    reorient(l);
+
+    MpMatrix lt(l);
+    transpose(lt);
+
+    std::cout << "m:\n" << m << std::endl;
+    std::cout << "l:\n" << l << std::endl;
+    std::cout << "lt:\n" << lt << std::endl;
+    std::cout << "diagonal: " << diagonal << std::endl;
+
+    MpMatrix d(dim, m_shift, ROW_ORIENTED);
+    impose_diagonal(diagonal, d);
+    std::cout << "ld:\n" << l << std::endl;
+
+    MpMatrix ld(dim, m_shift, ROW_ORIENTED);
+    multiply(l, d, ld);
+
+    MpMatrix ldlt(dim, m_shift, ROW_ORIENTED);
+    multiply(ld, lt, ldlt);
+    
+    std::cout << "ldlt:\n" << ldlt << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,24 @@
 #include <iostream>
 #include <gmpxx.h>
+#include "demo.hpp"
 #include "fixedmpz.hpp"
 #include "mpmatrix.hpp"
 #include "moment_algorithm.hpp"
 
 using namespace momentmp;
 
-void demo(MpMatrix &m, size_t dim, fmp_shift_t m_shift) {
-    std::cout << "original:\n";
-    std::cout << m;
-
-    cholesky_decompose(m);
-    std::cout << "cholesky:\n";
-    std::cout << m;
-
-    MpMatrix lt(m);
-    transpose(lt);
-    std::cout << "after transpose:\n";
-    std::cout << lt;
-
-    reorient(m);
-    std::cout << "reoriented:\n";
-    std::cout << m;
-
-    MpArray diagonals(dim, m_shift);
-    extract_diagonal(m, diagonals);
-    std::cout << "diagonals:\n";
-    std::cout << diagonals;
-    std::cout << "lower:\n";
-    std::cout << m;
-
-    // reorient(m);
-    invert(m);
-    std::cout << "inverted:\n";
-    std::cout << m;
-
-    invert_diagonal(diagonals);
-    std::cout << "diagonals inverted:\n";
-    std::cout << diagonals;
-
-    invert(m);
-    std::cout << "inverted inverted:\n";
-    std::cout << m;
-
-    invert_diagonal(diagonals);
-    std::cout << "diagonals inverted inverted:\n";
-    std::cout << diagonals;
+void print_matrix(double *matrix, size_t dim) {
+    std::cout << "\nAs doubles:\n";
+    for (size_t i = 0; i < (dim * dim); i++) {
+        if (i > 1 && i % dim == 0) {
+            std::cout << '\n';
+        } 
+        std::cout << matrix[i] << '\t';
+    }
+    std::cout << '\n';
 }
 
-void demo_multiply(MpMatrix &m, size_t dim, fmp_shift_t m_shift) {
-    MpMatrix l(m);
-    cholesky_decompose(l);
-    std::cout << "cholesky ld:\n" << l << std::endl;
-
-    MpArray diagonal(dim, m_shift);
-    extract_diagonal(l, diagonal);
-
-    reorient(l);
-
-    MpMatrix lt(l);
-    transpose(lt);
-
-    std::cout << "m:\n" << m << std::endl;
-    std::cout << "l:\n" << l << std::endl;
-    std::cout << "lt:\n" << lt << std::endl;
-    std::cout << "diagonal: " << diagonal << std::endl;
-
-    MpMatrix d(dim, m_shift, ROW_ORIENTED);
-    impose_diagonal(diagonal, d);
-    std::cout << "ld:\n" << l << std::endl;
-
-    MpMatrix ld(dim, m_shift, ROW_ORIENTED);
-    multiply(l, d, ld);
-
-    MpMatrix ldlt(dim, m_shift, ROW_ORIENTED);
-    multiply(ld, lt, ldlt);
-    
-    std::cout << "ldlt:\n" << ldlt << std::endl;
-}
-
-void the_project(MpMatrix &m) {
+void inversion(MpMatrix &m, MpMatrix &m_inverse) {
     auto dim = m.getDim();
     auto shift = m.getShift();
 
@@ -131,23 +70,9 @@ void the_project(MpMatrix &m) {
     // Now multiply together to get inverse of m
     MpMatrix ltd_inverse(dim, shift, ROW_ORIENTED);
     multiply(lt_inverse, d_inverse, ltd_inverse);
-
-    MpMatrix m_inverse(dim, shift, ROW_ORIENTED); // m' = (lt)'d'l'
-    multiply(ltd_inverse, l_inverse, m_inverse);
+    multiply(ltd_inverse, l_inverse, m_inverse);    // m' = (lt)'d'l'
 
     std::cout << "\nInverse of m:\n" << m_inverse;
-
-    double *m_inverse_as_doubles = new double[dim * dim];
-    to_double_array(m_inverse, m_inverse_as_doubles);
-
-    std::cout << "\nAs doubles:\n";
-    for (size_t i = 0; i < (dim * dim); i++) {
-        if (i > 1 && i % dim == 0) {
-            std::cout << '\n';
-        } 
-        std::cout << m_inverse_as_doubles[i] << '\t';
-    }
-    std::cout << '\n';
 }
 
 int main(int argc, char *argv[]) {
@@ -157,45 +82,15 @@ int main(int argc, char *argv[]) {
 
     fmp_shift_t m_shift = 256;
 
-    // size_t dim = 4;
-    // MpMatrix m(dim, m_shift);
-    // fixedmpz m_raw[] = {
-    //     18^256_fmpz, 22^256_fmpz,  54^256_fmpz,  42^256_fmpz,  
-    //     22^256_fmpz, 70^256_fmpz,  86^256_fmpz,  62^256_fmpz,
-    //     54^256_fmpz, 86^256_fmpz, 174^256_fmpz, 134^256_fmpz, 
-    //     42^256_fmpz, 62^256_fmpz, 134^256_fmpz, 106^256_fmpz
-    // };
-
-    // auto dim = 3;
-    // MpMatrix m(dim, m_shift);
-    // fixedmpz m_raw[] = {
-    //     4^256_fmpz,     0^256_fmpz,     0^256_fmpz, 
-    //     12^256_fmpz,    37^256_fmpz,    0^256_fmpz,
-    //     -(16^256_fmpz), -(43^256_fmpz), 98^256_fmpz 
-    // };
-
-    // size_t dim = 4;
-    // MpMatrix m(dim, m_shift);
-    // fixedmpz m_raw[] = {
-    //     1^256_fmpz, 0^256_fmpz, 0^256_fmpz, 0^256_fmpz,  
-    //     4^256_fmpz, 1^256_fmpz, 0^256_fmpz, 0^256_fmpz,
-    //     6^256_fmpz, 2^256_fmpz, 1^256_fmpz, 0^256_fmpz, 
-    //     3^256_fmpz, 7^256_fmpz, 4^256_fmpz, 1^256_fmpz
-    // };
-
-    // for (size_t col = 0; col < dim; col++) {
-    //     for (size_t row = 0; row < dim; row++) {
-    //         m[col][row] = m_raw[row * dim + col];
-    //     }
-    // }
-
-    // demo(m, dim, m_shift);
-    // demo_multiply(m, dim, m_shift);
-
     auto dim = 3;
-    MpMatrix m(dim, m_shift);
+    MpMatrix m(dim, m_shift, COL_ORIENTED), m_inverse(dim, m_shift, ROW_ORIENTED);
 
-    the_project(m);
+    inversion(m, m_inverse);
+
+    double *m_inverse_as_doubles = new double[dim * dim];
+    to_double_array(m_inverse, m_inverse_as_doubles);
+
+    print_matrix(m_inverse_as_doubles, dim);
 
     return 0;
 }
